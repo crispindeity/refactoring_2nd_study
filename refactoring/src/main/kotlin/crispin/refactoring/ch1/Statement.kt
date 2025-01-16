a8591affdb8a8ea0ab0208db40d5bf52a162746d
crispin <h.c.shin.dev09@gmail.com>
2025-01-16 22:02:10 +0900

    :recycle: Refactor: amountFor() 함수 이동, EnrichedPerformance 클래스 amount 추가
    (cherry picked from commit 3b2493bf414edc32a83c610b4d3a02149a6cdc8f)

--- refactoring/src/main/kotlin/crispin/refactoring/ch1/Statement.kt
+++ refactoring/src/main/kotlin/crispin/refactoring/ch1/Statement.kt
@@ -12,35 +12,15 @@ data class EnrichedPerformance(
     val playID: String,
     val audience: Int,
     val play: Play
-)
+) {
+    var amount: Int = 0
+}
 
 fun statement(
     invoice: Invoice,
     plays: Map<String, Play>
 ): String {
     fun playFor(aPerformance: Performance): Play = plays[aPerformance.playID]!!
-
-    fun enrichPerformance(aPerformance: Performance): EnrichedPerformance =
-        EnrichedPerformance(
-            aPerformance.playID,
-            aPerformance.audience,
-            playFor(aPerformance)
-        )
-
-    val statementData =
-        StatementData(
-            invoice.customer,
-            invoice.performances.map { enrichPerformance(it) }
-        )
-
-    return renderPlainText(statementData)
-}
-
-private fun renderPlainText(data: StatementData): String {
-    fun krw(aNumber: Int): String? =
-        NumberFormat
-            .getCurrencyInstance(Locale.KOREA)
-            .format(aNumber / 100)
 
     fun amountFor(aPerformance: EnrichedPerformance): Int {
         var result: Int
@@ -65,6 +45,30 @@ private fun renderPlainText(data: StatementData): String {
         return result
     }
 
+    fun enrichPerformance(aPerformance: Performance): EnrichedPerformance =
+        EnrichedPerformance(
+            aPerformance.playID,
+            aPerformance.audience,
+            playFor(aPerformance)
+        ).apply {
+            amount = amountFor(this)
+        }
+
+    val statementData =
+        StatementData(
+            invoice.customer,
+            invoice.performances.map { enrichPerformance(it) }
+        )
+
+    return renderPlainText(statementData)
+}
+
+private fun renderPlainText(data: StatementData): String {
+    fun krw(aNumber: Int): String? =
+        NumberFormat
+            .getCurrencyInstance(Locale.KOREA)
+            .format(aNumber / 100)
+
     fun volumeCreditsFor(aPerformance: EnrichedPerformance): Int {
         var volumeCredits = 0
         volumeCredits += maxOf(aPerformance.audience - 30, 0)
@@ -76,7 +80,7 @@ private fun renderPlainText(data: StatementData): String {
     fun totalAmount(): Int {
         var result = 0
         for (performance: EnrichedPerformance in data.performances) {
-            result += amountFor(performance)
+            result += performance.amount
         }
         return result
     }
@@ -92,7 +96,7 @@ private fun renderPlainText(data: StatementData): String {
     var result = "청구 내역 (고객명: ${data.customer})\n"
     for (performance: EnrichedPerformance in data.performances) {
         result +=
-            "    ${performance.play.name}: ${krw(amountFor(performance))} " +
+            "    ${performance.play.name}: ${krw(performance.amount)} " +
             "(${performance.audience}석)\n"
     }
 
