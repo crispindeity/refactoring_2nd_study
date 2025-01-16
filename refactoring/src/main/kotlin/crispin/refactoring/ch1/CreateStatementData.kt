d449298ddb2efc9a984ec855d96b737d95a59a63
crispin <h.c.shin.dev09@gmail.com>
2025-01-16 22:45:06 +0900

    :recycle: Refactor: createStatementData 함수 파일 분리
    (cherry picked from commit 7b77f02302dddc23ce7252e26432374e8f2b3251)

---
+++ refactoring/src/main/kotlin/crispin/refactoring/ch1/CreateStatementData.kt
@@ -0,0 +1,81 @@
+package crispin.refactoring.ch1
+
+data class StatementData(
+    val customer: String,
+    val performances: List<EnrichedPerformance>
+) {
+    var totalAmount: Int = 0
+    var totalVolumeCredits: Int = 0
+}
+
+data class EnrichedPerformance(
+    val playID: String,
+    val audience: Int,
+    val play: Play
+) {
+    var amount: Int = 0
+    var volumeCredits: Int = 0
+}
+
+fun createStatementData(
+    invoice: Invoice,
+    plays: Map<String, Play>
+): StatementData {
+    fun playFor(aPerformance: Performance): Play = plays[aPerformance.playID]!!
+
+    fun amountFor(aPerformance: EnrichedPerformance): Int {
+        var result: Int
+        when (aPerformance.play.type) {
+            "tragedy" -> {
+                result = 40000
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+            }
+
+            "comedy" -> {
+                result = 30000
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20)
+                }
+                result += 300 * aPerformance.audience
+            }
+
+            else -> throw IllegalArgumentException("알 수 없는 장르: ${aPerformance.play.type}")
+        }
+        return result
+    }
+
+    fun volumeCreditsFor(aPerformance: EnrichedPerformance): Int {
+        var volumeCredits = 0
+        volumeCredits += maxOf(aPerformance.audience - 30, 0)
+
+        if ("comedy" == aPerformance.play.type) volumeCredits += aPerformance.audience / 5
+        return volumeCredits
+    }
+
+    fun totalAmount(data: StatementData): Int = data.performances.sumOf { it.amount }
+
+    fun totalVolumeCredits(data: StatementData): Int = data.performances.sumOf { it.volumeCredits }
+
+    fun enrichPerformance(aPerformance: Performance): EnrichedPerformance =
+        EnrichedPerformance(
+            aPerformance.playID,
+            aPerformance.audience,
+            playFor(aPerformance)
+        ).apply {
+            amount = amountFor(this)
+            volumeCredits = volumeCreditsFor(this)
+        }
+
+    val statementData =
+        StatementData(
+            invoice.customer,
+            invoice.performances.map { enrichPerformance(it) }
+        ).apply {
+            totalAmount = totalAmount(this)
+            totalVolumeCredits = totalVolumeCredits(this)
+        }
+
+    return statementData
+}
