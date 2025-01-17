9f40f5403bee98adfd348609a60c0dcc3553ca51
crispin <h.c.shin.dev09@gmail.com>
2025-01-17 10:43:20 +0900

    :white_check_mark: Test: htmlStatement() 함수 테스트 추가
    (cherry picked from commit 01ea6919b1e8acba3f0b08286e03470815ef6d03)

--- refactoring/src/test/kotlin/crispin/refactoring/ch1/HtmlStatementTest.kt
+++ refactoring/src/test/kotlin/crispin/refactoring/ch1/HtmlStatementTest.kt
@@ -1,4 +1,30 @@
 package crispin.refactoring.ch1
 
-import org.junit.jupiter.api.Assertions.*
- class HtmlStatementTest
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class HtmlStatementTest :
+    FunSpec({
+        test("청구서 Html 생성 테스트") {
+            // given
+            val invoice: Invoice = invoices[0]
+            val plays: Map<String, Play> = plays
+
+            // when
+            val result: String = htmlStatement(invoice, plays)
+
+            // then
+            result shouldBe
+                """
+                <h1>청구 내역 (고객명: BigCo)</h1>
+                <table>
+                <tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>    <tr><td>Hamlet</td><td>(55석)</td><td>₩650</td></tr>
+                    <tr><td>As You Like It</td><td>(35석)</td><td>₩580</td></tr>
+                    <tr><td>Othello</td><td>(40석)</td><td>₩500</td></tr>
+                </table>
+                <p>총액: <em>₩1,730</em></p>
+                <p>적립 포인트: <em>47</em>점</p>
+                
+                """.trimIndent()
+        }
+    })
